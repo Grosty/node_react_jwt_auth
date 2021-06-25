49e5c4c71e7880df3217da3471efe0667b94ffb6
Grosty <grostmaster@gmail.com>
2021-06-26 00:17:18 +0300

    init client react ts

---
+++ client/src/App.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+const App: FC = () => {
+  return <div className="App">123</div>;
+};
+
+export default App;
